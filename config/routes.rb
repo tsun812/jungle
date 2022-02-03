8672233b50c64b98e6aef1323513100958479d06
Yan Sun <tsun812@gmail.com>
2022-02-03 10:27:16 -0800

    create controller and view for about page, add about to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :about, only: [:index]
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
